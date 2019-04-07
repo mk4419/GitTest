220579470b6655debc6af6b6f2d6c933387094e1
mk4419 <shiyou105@gmail.com>
2019-04-07 15:28:39 +0900

    コメhoge

--- gittest/ViewController.swift
+++ gittest/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //hoge
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
